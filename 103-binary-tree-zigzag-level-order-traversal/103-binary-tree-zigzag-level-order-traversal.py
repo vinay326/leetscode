9d7487147cfe0c121da1e24c5581de65bb9a03f4
vinay326 <55488528+vinay326@users.noreply.github.com>
2022-07-18 18:59:08 -0600

    Time: 39 ms (80.68%), Space: 14.2 MB (14.60%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -7,25 +7,31 @@
 
 from collections import deque
 class Solution:
-    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:        
+        result = []
         if root is None:
-            return []
-
-        results = []
-        def dfs(node, level):
-            if level >= len(results):
-                results.append(deque([node.val]))
-            else:
-                if level % 2 == 0:
-                    results[level].append(node.val)
+            return result
+        q = []
+        q.append(root)
+        q.append(None)
+        levels = deque()
+        traversal = True
+        while len(q) > 0:
+            curr_node = q.pop(0)
+            if curr_node:
+                if traversal:
+                    levels.append(curr_node.val)
                 else:
-                    results[level].appendleft(node.val)
-
-            for next_node in [node.left, node.right]:
-                if next_node is not None:
-                    dfs(next_node, level+1)
-
-        # normal level order traversal with DFS
-        dfs(root, 0)
-
-        return results        
+                    levels.appendleft(curr_node.val)
+                
+                if curr_node.left:
+                    q.append(curr_node.left)
+                if curr_node.right:
+                    q.append(curr_node.right)
+            else:
+                result.append(levels)
+                if len(q) > 0:
+                    q.append(None)
+                levels = deque()
+                traversal = not traversal
+        return result
