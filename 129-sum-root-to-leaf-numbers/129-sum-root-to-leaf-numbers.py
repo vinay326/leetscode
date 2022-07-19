a8c1ba31c16b4b108b8f0bec83af648cfb9c8649
vinay326 <55488528+vinay326@users.noreply.github.com>
2022-07-19 12:27:13 -0600

    Time: 57 ms (25.68%), Space: 13.8 MB (67.13%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -7,18 +7,18 @@
 from collections import deque
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        s = [(root, 0)]
-        result_sum = 0
-        while len(s) > 0:
-            curr_node, r = s.pop()
-            r = r * 10 + curr_node.val
-            
-            if curr_node.right is None and curr_node.left is None:
-                     result_sum += r
-            
-            if curr_node.right:
-                s.append((curr_node.right, r))
-            if curr_node.left:
-                s.append((curr_node.left, r))
         
-        return result_sum
+            def dfs(curr_node, r, result_sum):
+                if curr_node is None:
+                    return result_sum
+                    
+                r = r * 10 + curr_node.val
+                
+                if curr_node.right is None and curr_node.left is None:
+                         result_sum += r
+                         return result_sum
+
+                return dfs(curr_node.left,r, result_sum) + dfs(curr_node.right, r, result_sum)
+            x=dfs(root, 0, 0)
+            return x
+        
