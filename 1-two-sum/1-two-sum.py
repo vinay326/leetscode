ffbf7160071a53e7d95459d1ddaa81cccb383e60
vinay326 <55488528+vinay326@users.noreply.github.com>
2022-07-19 20:52:42 -0600

    Time: 124 ms (42.50%), Space: 15.2 MB (24.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = {}
-        for p in range(len(nums)):
-            char = nums[p]
-            if (target - char)  in result:
-                return [result[target-char], p]
+        for i in range(len(nums)):
+            char = nums[i]
+            if (target-char) in result:
+                return [i,result[target-char]]
             if char not in result:
-                result[char] = p
+                result[char] = i
 
-        
+            
